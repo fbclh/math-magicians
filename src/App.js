02738208632979a79916b05be09319ab110dbd56
Fabio Coelho <fbclh@protonmail.com>
2022-04-21 22:15:55 -0300

    Convert class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Calculator from './components/Calculator';
+import { Calculator } from './components/Calculator';
 import './styles/Calculator.css';
 import './styles/index.css';
 
