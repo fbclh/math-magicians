4fe90fc4e3a7907073a5c623f5d4d7b4384787e9
Fabio Coelho <fbclh@protonmail.com>
2022-04-27 12:49:44 -0300

    Run test for navbar

--- src/__tests__/navbar.test.js
+++ src/__tests__/navbar.test.js
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Navbar from '../components/Navbar';
 
 it('Check if Navbar page renders correctly', () => {
-  const tree = renderer.create(<Navbar />);
+  const tree = renderer.create(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>,
+  );
   expect(tree).toMatchSnapshot();
 });
