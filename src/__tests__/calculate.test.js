92b76fcc6e1c20e6198040176bcb76a7755cdd25
Fabio Coelho <fbclh@protonmail.com>
2022-04-27 12:34:19 -0300

    Run test calculate sum

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,13 +1,13 @@
-// import Calculate from '../logic/Calculate';
+import Calculate from '../logic/Calculate';
 
-// describe('Test Calculate function with calculations to check the results are correct.', () => {
-//   test('15 + 3 = 18', () => {
-//     const object = {
-//       total: 15,
-//       next: 3,
-//       operation: '+',
-//     };
-//     const result = Calculate(object, '=');
-//     expect(result.total).toBe('18');
-//   });
-// });
+describe('Test Calculate function with calculations to check the results are correct.', () => {
+  test('15 + 3 = 18', () => {
+    const object = {
+      total: 15,
+      next: 3,
+      operation: '+',
+    };
+    const result = Calculate(object, '=');
+    expect(result.total).toBe('18');
+  });
+});
