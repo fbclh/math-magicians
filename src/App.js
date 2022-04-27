844f15974e3b3ee98996a02e6a8161b71385d11b
Fabio Coelho <fbclh@protonmail.com>
2022-04-26 21:01:17 -0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { Component } from 'react';
-import { Calculator } from './components/Calculator';
-import './styles/Calculator.css';
-import './styles/index.css';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
+import NavBar from './components/NavBar';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => (
+  <>
+    <NavBar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/Calculator" element={<Calculator />} />
+      <Route path="/Quotes" element={<Quotes />} />
+    </Routes>
+  </>
+);
 
 export default App;
