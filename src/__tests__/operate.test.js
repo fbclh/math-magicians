5755117b63e6a077af4628b450b88bc1de15a01a
Alexr16 <j.alex-1996@hotmail.com>
2022-04-27 10:25:21 -0500

    Add sum and rest operate test

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,13 +1,23 @@
-// import Operate from '../logic/Operate';
+import Operate from '../logic/Operate';
 
-// describe('Check the Operate results are correct', () => {
-//   test('5 + 3 = 8', () => {
-//     const obj = {
-//       numberOne: 5,
-//       numberTwo: 3,
-//       operation: '+',
-//     };
-//     const result = Operate(obj.numberOne, obj.numberTwo, obj.operation);
-//     expect(result).toBe('8');
-//   });
-// });
+describe('Check the Operate results are correct', () => {
+  test('5 + 3 = 8', () => {
+    const obj = {
+      numberOne: 5,
+      numberTwo: 3,
+      operation: '+',
+    };
+    const result = Operate(obj.numberOne, obj.numberTwo, obj.operation);
+    expect(result).toBe('8');
+  });
+
+  test('5 - 3 = 2', () => {
+    const obj = {
+      numberOne: 5,
+      numberTwo: 3,
+      operation: '-',
+    };
+    const result = Operate(obj.numberOne, obj.numberTwo, obj.operation);
+    expect(result).toBe('2');
+  });
+});
