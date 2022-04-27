47b6debc0855a4d5e899a47240e36ed956e673e2
Alexr16 <j.alex-1996@hotmail.com>
2022-04-27 11:00:06 -0500

    Fix linter errors

--- src/__tests__/navbar.test.js
+++ src/__tests__/navbar.test.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 
 it('Check if Navbar page renders correctly', () => {
   const tree = renderer.create(
